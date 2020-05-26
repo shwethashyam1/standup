9ee4b1312a6ca3027a6e6fa2a13bfd654efecb94
Douglas Duteil <douglasduteil@gmail.com>
2020-05-26 13:54:48 +0200

    build(k8s): add env config map for www

--- .k8s/components/www.ts
+++ .k8s/components/www.ts
@@ -1,5 +1,28 @@
-import env from "@kosko/env";
-import { create } from "@socialgouv/kosko-charts/components/app";
+import env from "@kosko/env"
+import { create } from "@socialgouv/kosko-charts/components/app"
+import { ConfigMap } from "kubernetes-models/v1/ConfigMap"
 
-const { deployment, ingress, service } = create(env.component("www"));
-export default [deployment, ingress, service];
+const params = env.component("www");
+const { deployment, ingress, service } = create(params)
+
+const envConfigMap = new ConfigMap({
+  metadata: {
+    labels: params.labels,
+    name: `${params.name}-env`,
+  },
+  data: {
+    GITHUB_ORGANIZATION: process.env.GITHUB_ORGANIZATION || "SocialGouv",
+    HASURA_URL: process.env.HASURA_URL || "http://hasura",
+    NODE_ENV: "production",
+  }
+})
+
+deployment.spec!.template.spec!.containers[0].envFrom = [
+  {
+    configMapRef: {
+      name: envConfigMap.metadata!.name
+    }
+  }
+]
+
+export default [deployment, ingress, service, envConfigMap]
